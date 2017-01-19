3b10e0b40a02b3ad70f7d431fe2994ce0dc42f19
Michael Richards <mrichards@Michaels-MacBook-Pro-2.local>
2017-01-19 14:18:17 -0800

    Updated CommentsList to sync with new comments components

---
+++ src/components/CommentsList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="comments">
+    <demo-comment v-for="comment in comments">
+      {{comment}}
+    </demo-comment>
+  </div>
+</template>
+
+<script>
+  import DemoComment from './Comment.vue';
+
+  export default {
+    props: ['comments', 'commentcount'],
+    components: {
+      demoComment: DemoComment,
+    }
+  }
+</script>
