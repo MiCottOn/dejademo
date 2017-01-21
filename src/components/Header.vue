1cc47cd18830090445f56facd56b27a376ad7771
Michael Richards <mrichards@Michaels-MacBook-Pro-2.local>
2017-01-20 17:02:09 -0800

    Updated Header component with clear posts event

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,18 @@
     <h1>DejaDemo</h1>
     <nav>
       <ul>
-        <li><a href="">Profile</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#" @click="clearPosts">Clear Posts</a></li>
       </ul>
     </nav>
   </header>
 </template>
+<script>
+  export default {
+    methods: {
+      clearPosts() {
+        this.$emit('clearPosts')
+      }
+    }
+  }
+</script>
